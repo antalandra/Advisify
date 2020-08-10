b698a360e178a073e6211965fae9d6d7663b93ee
antalandra <andra.antalberbecaru@ucdconnect.ie>
2020-08-10 13:45:53 +0100

    Created Search, Advice and Likes models. Made api request for search results in index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,26 @@
 // GLOBAL APP CONTROLLER
+import Search from './models/Search';
+
+
+// GLOBAL STATE OF THE APP
+// Contains:
+// 1. Search Object
+const state = {};
+
+// SEARCH CONTROLLER
+
+async function controlSearch() {
+    // state.search = new Search('spiders');
+
+    try {
+        await state.search.getResults();
+        console.log(state.search.result);
+    }
+    catch(error) {
+        alert('Error processing search request.');
+    }
+
+}
+
+controlSearch();
+
---
+++ src/js/models/Search.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+export default class Search {
+    constructor(query){
+        this.query = query;
+    }
+
+    async getResults() {
+        try {
+            const apiResult = await axios(`https://api.adviceslip.com/advice/search/${this.query}`);
+            this.result = apiResult.data.slips;
+            //console.log(apiResult);
+        }
+        catch(error){
+            console.log(error);
+        }
+    };
+}
